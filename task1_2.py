1478c24d035093a3010befaa1dadd3bb60ea1e9f
namanshah22 <93568656+namanshah22@users.noreply.github.com>
2022-09-17 20:22:41 +0530

    Add files via upload

---
+++ task1_2.py
@@ -0,0 +1,8 @@
+creepy_doll = ['red_light', 'green_light', 'red_light', 'you_got_shot', 'green_light', 'you_got_shot',
+'you_got_shot', 'green_light', 'you_ got_shot', 'red_light', 'green_light', 'red_light', 'you_got_shot',
+'green_light','red_light', 'red_light', 'green_light', 'you_got_shot','red_light', 'you_got_shot']
+#'next_game'
+#which contains all 'green_light' elements from the given list.
+#1,4,7,10,13,16
+next_doll=creepy_doll[1:19:3]
+print(next_doll)
